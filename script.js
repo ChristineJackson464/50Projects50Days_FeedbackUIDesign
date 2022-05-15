3837fc2c5e8d29f0b9dc2a4d530a6d603900f77d
ChristineJackson464 <mom2mb464@gmail.com>
2022-05-15 11:59:32 -0500

    Submitting Day 43 CodeAlong

---
+++ script.js
@@ -0,0 +1,30 @@
+const ratings = document.querySelectorAll('.rating')
+const ratingsContainer = document.querySelector('.ratings-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+// Event bubbling
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+})
+
+sendBtn.addEventListener('click', (e) => {
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Keep it Going!</strong>
+    <br>
+    <strong>Progress: ${selectedRating}</strong>
+    <p>Each Day is YOUR DAY</p>
+    `
+})
+
+function removeActive() {
+    for(let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
